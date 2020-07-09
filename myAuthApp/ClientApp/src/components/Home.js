b519431a0d8bf1fa84c42a056383e74926d7e293
Zachary Moorman <zachary.moorman@hcss.com>
2020-07-08 21:13:31 -0500

    Initial commit

---
+++ myAuthApp/ClientApp/src/components/Home.js
@@ -0,0 +1,52 @@
+import React, { Component, useState } from 'react';
+
+export const Home = (props) => {
+
+  const [showLogin, setShowLogin] = useState(true);
+  const [loginInfo, setLoginInfo] = useState({userName:'', password:''})
+  const [signUpInfo, setSignUpInfo] = useState({userName:'', password:'', email:''})
+
+  return (
+    <div className="login-page">
+      <div className="form">
+        { showLogin &&  
+          <LoginForm  
+            loginInfo={loginInfo} 
+            setLoginInfo={setLoginInfo}
+            setShowLogin={setShowLogin }
+            />
+        }
+        { !showLogin && 
+          <SignUpForm
+            signUpInfo={signUpInfo} 
+            setSignUpInfo={setSignUpInfo}
+            setShowLogin={setShowLogin }
+            />       
+        }       
+      </div>
+    </div>
+  )
+}
+
+const LoginForm = (props) => {
+  return(
+    <div className="login-form">
+      <input type="text" placeholder="username"/>
+      <input type="password" placeholder="password"/>
+      <button>login</button>
+      <p className="message">Not registered? <a onClick={() => props.setShowLogin(false)}>Create an account</a></p>
+    </div>
+  )
+}
+
+const SignUpForm = (props) => {
+  return (
+    <div className="register-form">
+      <input type="text" placeholder="name"/>
+      <input type="password" placeholder="password"/>
+      <input type="text" placeholder="email address"/>
+      <button>create</button>
+      <p className="message">Already registered? <a onClick={() => props.setShowLogin(true)}>Sign In</a></p>
+    </div>
+  )
+}
