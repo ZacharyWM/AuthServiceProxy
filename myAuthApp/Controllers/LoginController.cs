661b15821fc1a8d1a3a4901538846c25dedd83f4
Zachary Moorman <zachary.moorman@hcss.com>
2020-07-19 21:21:03 -0500

    Add GetClaim method

--- myAuthApp/Controllers/LoginController.cs
+++ myAuthApp/Controllers/LoginController.cs
@@ -99,6 +99,15 @@ namespace myAuthApp.Controllers
             }
             return true;
         }
+
+        public string GetClaim(string token, string claimType)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var securityToken = tokenHandler.ReadToken(token) as JwtSecurityToken;
+
+            var stringClaimValue = securityToken.Claims.First(claim => claim.Type == claimType).Value;
+            return stringClaimValue;
+        }
         
     }
 
