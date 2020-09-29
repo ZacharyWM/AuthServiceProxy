bcad2189c85a207896ecbaa52c0085460b3b2a09
Zachary Moorman <zachary.moorman@hcss.com>
2020-09-29 09:22:07 -0500

    Mostly got the animation working.

--- myAuthApp/ClientApp/src/components/Authorizing.js
+++ myAuthApp/ClientApp/src/components/Authorizing.js
@@ -4,133 +4,142 @@ import queryString from 'query-string'
 import axios from 'axios'
 import config from './../config/appConfigService'
 
+
+//https://localhost:5001?redirect_uri=https://www.hcss.com/
 export const Authorizing = (props) => {
     const history = useHistory()
 
-    let size = 20
+    let yOffset = 40
+    let xOffset = 40
+
+    let movingClockwise = false
+
     let radius = 10
 
-    let yMin = 40
-    let yMax = yMin + size
-    let yMidpoint = (yMin + yMax) / 2
-
-    let xMin = 40
-    let xMax = xMin + size
-    let xMidpoint = (xMin + xMax) / 2
 
 
-    // let [x, setX] = useState(xMin)
-    // let [y, setY] = useState(yMin)
-    let [coordinates, setCoordinates] = useState({x: xMin, y: yMin})
-    const changeSize = 0.05
+    let [coordinates, setCoordinates] = useState({x: radius, y: 0, quadrant: 1})
+
+    const incrementSize = 1
 
    // const [angle, setAngle] = useState(90)
 
-   let getNextX = (currentX, change) => {
-        let nextX = currentX + change
-        if(nextX > xMax) {
-            nextX = xMax
-        }
-        if(nextX < xMin){
-            nextX = xMin
-        }
-        return nextX
-   } 
 
-   let solveForY = (x) => Math.sqrt(Math.pow(radius, 2) - Math.pow((x - 50),2)) + 50
+
+
+   let getY = (x) => Math.sqrt(Math.pow(radius, 2) - Math.pow(x, 2))
 
     let styling = {
         position: 'fixed', /* or absolute */
-        top: '30%', // min 30, max 50
-        left: '40%' // min 40, max 60
+        top: `${yOffset}%`, 
+        left: `${xOffset + radius}%` 
     }
 
-   // console.log(`x: ${coordinates.x}, y: ${coordinates.y}`)
 
 
     useEffect(() => {
 
         const interval = setInterval(() => {
 
-            let quadrant_1 = coordinates.x <= xMidpoint && coordinates.y >= yMidpoint
-            let quadrant_2 = coordinates.x > xMidpoint && coordinates.y > yMidpoint
-            let quadrant_3 = coordinates.x >= xMidpoint && coordinates.y <= yMidpoint
-            let quadrant_4 = coordinates.x < xMidpoint && coordinates.y <= yMidpoint
+            // TODO: When X is close to 0, the jump for Y is very large. 
+            // Need to modulate the X increment to keep a constant angular velocity
 
-
-
-            if(quadrant_1){
-                console.log('quad 1')
-                let nextX = getNextX(coordinates.x, changeSize)
-                let nextY = solveForY(nextX)
-                setCoordinates(coordinates => ({x: nextX, y: nextY}))
-            }
-            else if(quadrant_2){
-                console.log('quad 2')
-                let nextX = getNextX(coordinates.x, changeSize)
-                let nextY = solveForY(nextX)
-                setCoordinates(coordinates => ({x: nextX, y: nextY}))
-            }
-            else if(quadrant_3){
-                console.log('quad 3')
-
-                let nextX = getNextX(coordinates.x, -changeSize)
-                let nextY = solveForY(nextX)
-                nextY = nextY - (nextY - yMidpoint)
-                setCoordinates(coordinates => ({x: nextX, y: nextY}))
-            }
-            else if (quadrant_4){
-                console.log('quad 4')
-                let nextX = getNextX(coordinates.x, changeSize)
-                let nextY = solveForY(nextX)
-                setCoordinates(coordinates => ({x: nextX, y: nextY}))
+            let nextX = coordinates.x
+            let nextY = -(coordinates.y) // use the inverse since we are measuring from the top
+            let quadrant = coordinates.quadrant
+            
+            switch (quadrant) {
+                case 1:
+                    nextX -= incrementSize
+                    nextY = -Math.abs(getY(nextX))
+                    quadrant = (nextX > 0) ? 1 : 2
+                    break
+                case 2:
+                    nextX -= incrementSize
+                    nextY = -(Math.abs(getY(nextX)))
+                    quadrant = (Math.abs(nextX) < radius) ? 2 : 3
+                    break
+                case 3:
+                    nextX += incrementSize
+                    nextY = (Math.abs(getY(nextX)))
+                    quadrant = (nextX < 0) ? 3 : 4    
+                    break
+                case 4:
+                    nextX += incrementSize
+                    nextY = Math.abs(getY(nextX))
+                    quadrant = (nextX < radius) ? 4 : 1      
+                    break              
+                default:
+                    break
             }
 
-        }, 10);
+            setCoordinates(() => ({
+                x: nextX, 
+                y: nextY, 
+                quadrant: quadrant
+            }))
+
+
+
+            /*
+                                |
+                        2 (-,+) |  1 (+,+)
+                                |         
+                         ----------------
+                                |        
+                        3 (-,-) |  4 (+,-)
+                                |          
+             */
+
+
+             console.log(`x: ${coordinates.x}, y: ${coordinates.y}, quad: ${quadrant}`)
+
+
+        }, 50);
         return () => clearInterval(interval);
 
     },[coordinates])
 
-    styling.top = `${coordinates.y}%`
-    styling.left = `${coordinates.x}%`
+    styling.top = `${(coordinates.y) + yOffset}%`
+    styling.left = `${coordinates.x + xOffset}%`
 
 
-    useEffect(() => {
+    // useEffect(() => {
 
-        // const { code, scope, state } = queryString.parse(props.location.search)
+    //     const { code, scope, state } = queryString.parse(props.location.search)
 
-        // const payload = {
-        //     "Code": code,
-        //     "Scope": scope,
-        //     "State": props.location.pathname,
-        //     "RedirectUri": config.redirectUri,
-        //     "ClientRedirectUri": JSON.parse(state).clientUri
-        // }
+    //     const payload = {
+    //         "Code": code,
+    //         "Scope": scope,
+    //         "State": props.location.pathname,
+    //         "RedirectUri": config.redirectUri,
+    //         "ClientRedirectUri": JSON.parse(state).clientUri
+    //     }
 
-        // axios.post("/auth/google", payload)
-        //      .then(response => {
-        //          const client_redirect_uri = response.data.client_redirect_uri
+    //     axios.post("/auth/google", payload)
+    //          .then(response => {
+    //              const client_redirect_uri = response.data.client_redirect_uri
 
-        //          if (client_redirect_uri && client_redirect_uri.length > 0){
-        //             window.location = client_redirect_uri
-        //          }
-        //          else {
-        //             history.push({
-        //                 pathname: '/dashboard',
-        //                 state: { authCode: response.data.auth_code }
-        //             })
-        //          }
+    //              if (client_redirect_uri && client_redirect_uri.length > 0){
+    //                 window.location = client_redirect_uri
+    //              }
+    //              else {
+    //                 history.push({
+    //                     pathname: '/dashboard',
+    //                     state: { authCode: response.data.auth_code }
+    //                 })
+    //              }
 
-        //      }).catch(error => {
-        //         console.log(error)
-        //      })
+    //          }).catch(error => {
+    //             console.log(error)
+    //          })
        
-    }, []) // should only run once
+    // }, []) // should only run once
 
 
     return (
         <Fragment>
-            <div className="primaryFont">Authorizing...</div>
+            <div className="primaryFont" >Authorizing...</div>
             <div className=" primaryFont" style={styling}>@</div>
         </Fragment>
 
